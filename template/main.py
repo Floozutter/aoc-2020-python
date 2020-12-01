15b521a1c325444dacc47294d1acaca957ec426e
Floozutter <floozutter@gmail.com>
2020-11-30 19:47:47 -0800

    Update template variable names

--- template/main.py
+++ template/main.py
@@ -2,6 +2,6 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 
 with open(INPUTPATH) as ifile:
-    lines = ifile.read()
+    filetext = ifile.read()
 
-data = [int(l) for l in lines.split()]
+data = [int(line) for line in filetext.split()]
