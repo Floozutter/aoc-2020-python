96f3e7f0c5e7ae3707a9e81efed96bfee0f5b384
Floozutter <floozutter@gmail.com>
2020-12-01 21:18:10 -0800

    Clean day02

--- day02/main.py
+++ day02/main.py
@@ -4,20 +4,15 @@ with open(INPUTPATH) as ifile:
     filetext = ifile.read()
 data = [line for line in filetext.split("\n") if line]
 
-valid = 0
-for line in data:
-	left, password = line.split(": ")
-	bounds, letter = left.split(" ")
-	a, b = map(int, bounds.split("-"))
-	if a <= password.count(letter) <= b:
-		valid += 1
-print(valid)
-
+valid1 = 0
 valid2 = 0
 for line in data:
-	left, password = line.split(": ")
-	bounds, letter = left.split(" ")
-	a, b = map(int, bounds.split("-"))
+	policy, password = line.split(": ")
+	numbers, letter = policy.split(" ")
+	a, b = map(int, numbers.split("-"))
+	if a <= password.count(letter) <= b:
+		valid1 += 1
 	if (password[a-1] == letter) + (password[b-1] == letter) == 1:
 		valid2 += 1
+print(valid1)
 print(valid2)
