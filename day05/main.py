52b630c4a78873c8f2da325a32710e026b2fb1fb
Floozutter <floozutter@gmail.com>
2020-12-04 21:49:15 -0800

    Clean day05

--- day05/main.py
+++ day05/main.py
@@ -4,28 +4,18 @@ with open(INPUTPATH) as ifile:
     filetext = ifile.read()
 data = [chunk for chunk in filetext.strip().split()]
 
-from typing import Tuple
-def seat(s: str) -> Tuple[int, int]:
-	row_low = 0
-	row_high = 128
+def seat(s: str) -> int:
+	row_range = [0, 128]
 	for c in s[:7]:
-		if c == "F":
-			row_high = (row_low + row_high) // 2
-		else:
-			row_low = (row_low + row_high) // 2
-	col_low = 0
-	col_high = 8
+		row_range[c == "F"] = sum(row_range) // 2
+	col_range = [0, 8]
 	for c in s[-3:]:
-		if c == "L":
-			col_high = (col_low + col_high) // 2
-		else:
-			col_low = (col_low + col_high) // 2
-	return (row_low, col_low)
+		col_range[c == "L"] = sum(col_range) // 2
+	return row_range[0] * 8 + col_range[0]
+seats = list(map(seat, data))
 
-print(max(map(lambda rc: rc[0] * 8 + rc[1], map(seat, data))))
+print(max(seats))
 
-plane = [["#"] * 8 for _ in range(128)]
-for rc in map(seat, data):
-	plane[rc[0]][rc[1]] = "."
-for i, row in enumerate(plane):
-	print(f"{''.join(row)} {i}")
+def arithmetic_series(first: int, last: int) -> int:
+	return (first + last) * (last - first + 1) // 2
+print(arithmetic_series(min(seats), max(seats)) - sum(seats))
