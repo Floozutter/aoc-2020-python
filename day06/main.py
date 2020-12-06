7afaf4cb0d4172891e8bd4407c3f8d8635bd40d4
Floozutter <floozutter@gmail.com>
2020-12-05 21:22:02 -0800

    Clean day06

--- day06/main.py
+++ day06/main.py
@@ -5,20 +5,12 @@ with open(INPUTPATH) as ifile:
 data = [chunk for chunk in filetext.strip().split("\n\n")]
 
 from collections import Counter
-counts1 = 0
-counts2 = 0
+any_yes = 0
+all_yes = 0
 for group in data:
-	answered1 = set()
-	answered2 = Counter()
 	people = list(group.split())
-	for person in people:
-		for c in person:
-			if c.isalpha():
-				answered1.add(c)
-				answered2[c] += 1
-	counts1 += len(answered1)
-	for k, v in answered2.items():
-		if v == len(people):
-			counts2 += 1
-print(counts1)
-print(counts2)
+	answers = Counter("".join(people))
+	any_yes += len(answers)
+	all_yes += sum(1 for yeses in answers.values() if yeses == len(people))
+print(any_yes)
+print(all_yes)
