026b8d81cfe0300b60b02e618f8c8146bafc5122
Floozutter <floozutter@gmail.com>
2020-12-05 23:49:06 -0800

    Use Dad's strategy for elegance

--- day06/main.py
+++ day06/main.py
@@ -4,13 +4,12 @@ with open(INPUTPATH) as ifile:
     filetext = ifile.read()
 data = [chunk for chunk in filetext.strip().split("\n\n")]
 
-from collections import Counter
-any_yes = 0
-all_yes = 0
+from functools import reduce
+in_any = 0
+in_all = 0
 for group in data:
-	people = list(group.split())
-	answers = Counter("".join(people))
-	any_yes += len(answers)
-	all_yes += sum(1 for yeses in answers.values() if yeses == len(people))
-print(any_yes)
-print(all_yes)
+	answer_sets = list(map(set, group.split()))
+	in_any += len(reduce(lambda a, b: a | b, answer_sets))
+	in_all += len(reduce(lambda a, b: a & b, answer_sets))
+print(in_any)
+print(in_all)
