5cf4ffba54ac5bd34773dc281a0f09dfc34b708f
Floozutter <floozutter@gmail.com>
2020-12-08 21:59:22 -0800

    Clean day09 (improve part 2)

--- day09/main.py
+++ day09/main.py
@@ -1,24 +1,33 @@
-INPUTPATH = "input.txt"
-#INPUTPATH = "input-test.txt"
+INPUTPATH, N = "input.txt", 25
+#INPUTPATH, N = "input-test.txt", 5
 with open(INPUTPATH) as ifile:
     filetext = ifile.read()
 data = [int(chunk) for chunk in filetext.strip().split()]
 
 from itertools import combinations
 from typing import List
-def valid(pre: List[int], z: int) -> bool:
-	return any(a + b == z for a, b in combinations(pre, 2))
-def first_invalid(data: List[int], preamble_length: int) -> bool:
+def valid(pre: List[int], n: int) -> bool:
+	return any(a + b == n for a, b in combinations(pre, 2))
+def first_invalid(data: List[int], preamble_length: int) -> int:
 	for i in range(preamble_length, len(data)):
 		if not valid(data[i - preamble_length: i], data[i]):
 			return data[i]
-target = first_invalid(data, 25)
+	return -1
+target = first_invalid(data, N)
 print(target)
 
-for length in range(2, len(data)):
-	for i in range(0, len(data) - length + 1):
-		sublist = data[i:i + length]
-		if sum(sublist) == target:
-			weakness = min(sublist) + max(sublist)
-			break
-print(weakness)
+def find_weakness(data: List[int], target: int) -> int:
+	for i, first in enumerate(data[:-1]):
+		total = first
+		smallest = first
+		largest = first
+		for n in data[i + 1:]:
+			smallest = n if n < smallest else smallest
+			largest = n if n > largest else largest
+			total += n
+			if total == target:
+				return smallest + largest
+			elif total > target:
+				break
+	return -1
+print(find_weakness(data, target))
