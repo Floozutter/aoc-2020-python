a42c20564731bf5161abe5fce808339b962e83ab
Floozutter <floozutter@gmail.com>
2020-12-11 21:38:06 -0800

    Clean day12

--- day12/main.py
+++ day12/main.py
@@ -4,27 +4,28 @@ with open(INPUTPATH) as ifile:
     filetext = ifile.read()
 data = [chunk for chunk in filetext.strip().split()]
 
-from typing import NamedTuple
+from typing import NamedTuple, List
 class Instruction(NamedTuple):
 	action: str
 	value: int
-from typing import List
-def run_route(route: List[Instruction]) -> int:
+route = [Instruction(s[0], int(s[1:])) for s in data]
+
+def run1(route: List[Instruction]) -> int:
 	x, y = 0, 0
 	heading = 0
 	for inst in route:
-		if inst.action == "N":
-			y += inst.value
-		elif inst.action == "S":
-			y -= inst.value
-		elif inst.action == "E":
+		if inst.action == "E":
 			x += inst.value
+		elif inst.action == "N":
+			y += inst.value
 		elif inst.action == "W":
 			x -= inst.value
+		elif inst.action == "S":
+			y -= inst.value
 		elif inst.action == "L":
-			heading += inst.value
+			heading = (heading + inst.value) % 360
 		elif inst.action == "R":
-			heading -= inst.value
+			heading = (heading - inst.value) % 360
 		elif inst.action == "F":
 			if heading == 0:
 				x += inst.value
@@ -34,27 +35,21 @@ def run_route(route: List[Instruction]) -> int:
 				x -= inst.value
 			elif heading == 270:
 				y -= inst.value
-			else:
-				assert False, "owo"
-		else:
-			assert False, "uwu"
-		heading = (360 + heading) % 360
 	return abs(x) + abs(y)
-route = [Instruction(s[0], int(s[1:])) for s in data]
-print(run_route(route))
+print(run1(route))
 
-def what(route: List[Instruction]) -> int:
+def run2(route: List[Instruction]) -> int:
 	x, y = 0, 0
 	a, b = 10, 1
 	for inst in route:
-		if inst.action == "N":
-			b += inst.value
-		elif inst.action == "S":
-			b -= inst.value
-		elif inst.action == "E":
+		if inst.action == "E":
 			a += inst.value
+		elif inst.action == "N":
+			b += inst.value
 		elif inst.action == "W":
 			a -= inst.value
+		elif inst.action == "S":
+			b -= inst.value
 		elif inst.action == "L":
 			for _ in range(inst.value // 90):
 				a, b = -b, a
@@ -64,7 +59,5 @@ def what(route: List[Instruction]) -> int:
 		elif inst.action == "F":
 			x += a * inst.value
 			y += b * inst.value
-		else:
-			assert False, "uwu"
 	return abs(x) + abs(y)
-print(what(route))
+print(run2(route))
