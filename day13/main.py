5a41fb3e2d931424953cf50ae6074ba404e4eada
Floozutter <floozutter@gmail.com>
2020-12-13 01:32:34 -0800

    Clean day13

--- day13/main.py
+++ day13/main.py
@@ -4,19 +4,20 @@ with open(INPUTPATH) as ifile:
     filetext = ifile.read()
 data = [chunk for chunk in filetext.strip().split()]
 
+arrival = int(data[0])
 buses = [int(s) if s != "x" else 0 for s in data[1].split(",")]
+def wait(arrival: int, bus: int) -> int:
+	return bus - arrival % bus
+buswaits = ((bus, wait(arrival, bus)) for bus in buses if bus)
+best_bus, best_wait = min(buswaits, key = lambda pair: pair[1])
+print(best_bus * best_wait)
 
-from typing import List
-from itertools import count
-def earliest(buses: List[int], start: int) -> int:
-	for timestamp in count(start):
-		for bus in buses:
-			if bus and timestamp % bus == 0:
-				return bus * (timestamp - start)
-print(earliest(buses, int(data[0])))
-
+from typing import NamedTuple
+class Congruence(NamedTuple):
+	mod: int
+	to: int
 from typing import Tuple
-def xgcd(a, b) -> Tuple[int, int]:
+def bezout_coefs(a: int, b: int) -> Tuple[int, int]:
 	r_last, r = a, b
 	s_last, s = 1, 0
 	t_last, t = 0, 1
@@ -26,17 +27,14 @@ def xgcd(a, b) -> Tuple[int, int]:
 		s_last, s = s, s_last - quotient * s
 		t_last, t = t, t_last - quotient * t
 	return s_last, t_last
-def help(a: int, b: int, c: int, d: int) -> Tuple[int, int]:
-	p = a * c
-	foo, bar = xgcd(a, c)
-	q = (foo * a * d + bar * c * b) % p
-	return p, q
-def what(buses: List[int]) -> int:
-	a, b = 1, 0
-	for i, bus in enumerate(buses):
-		if bus:
-			# (t + i) % bus = 0
-			c, d = bus, (bus - i) % bus
-			a, b = help(a, b, c, d)
-	return b
-print(what(buses))
+def combine_coprime(a: Congruence, b: Congruence) -> Congruence:
+	mod = a.mod * b.mod
+	x, y = bezout_coefs(a.mod, b.mod)
+	return Congruence(mod, (x * a.mod * b.to + y * b.mod * a.to) % mod)
+congruences = (
+	Congruence(bus, (bus - i) % bus)
+	for i, bus in enumerate(buses)
+	if bus
+)
+from functools import reduce
+print(reduce(combine_coprime, congruences).to)
