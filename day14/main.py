7365809094c5b8eb5788171dfc9670f278015982
Floozutter <floozutter@gmail.com>
2020-12-13 21:56:12 -0800

    Clean day14

--- day14/main.py
+++ day14/main.py
@@ -5,38 +5,33 @@ with open(INPUTPATH) as ifile:
 data = [chunk for chunk in filetext.strip().split("\n")]
 
 from typing import List
-def floating(address: str) -> List[int]:
-	for i, c in enumerate(address):
-		if c == "X":
-			a = list(address)
-			a[i] = "0"
-			a = "".join(a)
-			b = list(address)
-			b[i] = "1"
-			b = "".join(b)
-			return floating(a) + floating(b)
-	return [address]
+def ground(floating: str) -> List[str]:
+	for i, bit in enumerate(floating):
+		if bit == "X":
+			a = floating[: i] + "0" + floating[i + 1:]
+			b = floating[: i] + "1" + floating[i + 1:]
+			return ground(a) + ground(b)
+	return [floating]
 
-memory = dict()
+memory1 = dict()
 memory2 = dict()
 for line in data:
 	left, _, right = line.split()
 	if left == "mask":
 		mask = right
 	else:
-		# 1
 		address = int(left[left.index("[") + 1: left.index("]")])
-		value = list(f"{int(right):036b}")
-		for i, c in enumerate(mask):
-			if c != "X":
-				value[i] = c
-		memory[address] = int("".join(value), 2)
-		# 2
-		address2 = list(f"{address:036b}")
-		for i, c in enumerate(mask):
-			if c != "0":
-				address2[i] = c
-		for a in floating("".join(address2)):
-			memory2[int(a, 2)] = int(right)
-print(sum(memory.values()))
+		value = int(right)
+		# --- Part One ---
+		memory1[address] = int("".join(
+			value_bit if mask_bit == "X" else mask_bit
+			for value_bit, mask_bit in zip(f"{value:036b}", mask)
+		), 2)
+		# --- Part Two ---
+		for grounded in ground("".join(
+			address_bit if mask_bit == "0" else mask_bit
+			for address_bit, mask_bit in zip(f"{address:036b}", mask)
+		)):
+			memory2[grounded] = value
+print(sum(memory1.values()))
 print(sum(memory2.values()))
