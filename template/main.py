8140d255c8ecf083708cd961ff844688d0ecf069
Floozutter <floozutter@gmail.com>
2020-12-15 13:03:46 -0800

    Empty expectation

--- template/main.py
+++ template/main.py
@@ -2,4 +2,4 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     filetext = ifile.read()
-data = [int(chunk) for chunk in filetext.strip().split()]
+data = [chunk for chunk in filetext.strip().split()]
