a92eab13d9afe949b20bbf854e1432588ede7578
Floozutter <floozutter@gmail.com>
2020-12-23 09:07:06 -0800

    Make a circle of cups

--- day23/main.py
+++ day23/main.py
@@ -1,24 +1,34 @@
 INPUTPATH = "input.txt"
-#INPUTPATH = "input-test.txt"
+INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     filetext = ifile.read()
-cups = list(map(int, filetext.strip()))
+labels = list(map(int, filetext.strip()))
 
-curr = cups[0]
-for _ in range(100):
-	picked = []
-	for _ in range(3):
-		picked.append(cups.pop((cups.index(curr) + 1) % len(cups)))
-	dest = curr - 1
-	while dest not in cups:
-		if dest < min(cups):
-			dest = max(cups)
+from typing import Optional
+class Cup:
+	label: int
+	succ: "Cup"
+	def __init__(self, label: int, succ: Optional["Cup"] = None) -> None:
+		self.label = label
+		if succ is None:
+			self.succ = self
 		else:
-			dest -= 1
-	print(curr, cups, picked, dest)
-	while picked:
-		cups.insert(cups.index(dest) + 1, picked.pop())
-	curr = cups[(cups.index(curr) + 1) % len(cups)]
-print("".join(map(str, (
-	cups[(cups.index(1) + 1 + i) % len(cups)] for i in range(len(cups) - 1)
-))))
+			self.succ = succ
+	def insert_succ(self, label: int) -> None:
+		self.succ = Cup(label, self.succ)
+	def remove_succ(self) -> int:
+		removed = self.succ
+		self.succ = removed.succ
+		return removed.label
+from typing import Iterator
+def make_circle(labels: Iterator[int]) -> Cup:
+	reversed_labels = iter(reversed(labels))
+	last = Cup(next(reversed_labels))
+	print(last.label)
+	head = last
+	for label in reversed_labels:
+		print(label)
+		head = Cup(label, head)
+	last.succ = head
+	return head
+head = make_circle(labels)
