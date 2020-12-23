72ec8240c63893443a75f211e4de08f21c12e895
Floozutter <floozutter@gmail.com>
2020-12-23 13:10:14 -0800

    Add day23 part 2

--- day23/main.py
+++ day23/main.py
@@ -1,10 +1,10 @@
 INPUTPATH = "input.txt"
-INPUTPATH = "input-test.txt"
+#INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     filetext = ifile.read()
 labels = list(map(int, filetext.strip()))
 
-from typing import Optional
+from typing import Optional, Iterator, Iterable
 class Cup:
 	label: int
 	succ: "Cup"
@@ -20,15 +20,50 @@ class Cup:
 		removed = self.succ
 		self.succ = removed.succ
 		return removed.label
-from typing import Iterator
-def make_circle(labels: Iterator[int]) -> Cup:
-	reversed_labels = iter(reversed(labels))
-	last = Cup(next(reversed_labels))
-	print(last.label)
-	head = last
+	def labels(self) -> Iterator[int]:
+		yield self.label
+		cup = self.succ
+		while cup != self:
+			yield cup.label
+			cup = cup.succ
+	def cups(self) -> Iterator["Cup"]:
+		yield self
+		cup = self.succ
+		while cup != self:
+			yield cup
+			cup = cup.succ
+def make_circle(labels: Iterable[int]) -> Cup:
+	reversed_labels = iter(reversed(list(labels)))
+	head = last = Cup(next(reversed_labels))
 	for label in reversed_labels:
-		print(label)
 		head = Cup(label, head)
 	last.succ = head
 	return head
-head = make_circle(labels)
+def play(head: Cup, moves: int) -> Cup:
+	cups = dict((cup.label, cup) for cup in head.cups())
+	lowest, highest = min(cups), max(cups)
+	for _ in range(moves):
+		picks = []
+		for _ in range(3):
+			label = head.remove_succ()
+			del cups[label]
+			picks.append(label)
+		dest_label = head.label - 1
+		while (a := dest_label < lowest) or (b := dest_label in picks):
+			if a: dest_label = highest
+			else: dest_label -= 1
+		dest = cups[dest_label]
+		while picks:
+			label = picks.pop()
+			dest.insert_succ(label)
+			cups[label] = dest.succ
+		head = head.succ
+	return cups[1]
+
+print("".join(map(str, play(make_circle(labels), 100).labels()))[1:])
+
+highest = max(labels)
+from itertools import islice, chain, count
+head = make_circle(islice(chain(labels, count(highest + 1)), 1_000_000))
+head = play(head, 10_000_000)
+print(head.succ.label * head.succ.succ.label)
