c73e39dd89cb126e34285dcc85446fc37dab01e4
Floozutter <floozutter@gmail.com>
2020-12-24 11:21:22 -0800

    Add day24 part 2

--- day24/main.py
+++ day24/main.py
@@ -4,7 +4,7 @@ with open(INPUTPATH) as ifile:
     filetext = ifile.read()
 data = [chunk for chunk in filetext.strip().split("\n")]
 
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Set, Iterator, cast
 Coordinate = Tuple[int, int, int]
 DIRECTIONS: Dict[str, Coordinate] = {
 	"e" : ( 1, -1,  0),
@@ -14,6 +14,7 @@ DIRECTIONS: Dict[str, Coordinate] = {
 	"sw": (-1,  0,  1),
 	"se": ( 0, -1,  1)
 }
+
 def line_to_coordinate(line: str) -> Coordinate:
 	x, y, z = 0, 0, 0
 	it = iter(line)
@@ -23,8 +24,22 @@ def line_to_coordinate(line: str) -> Coordinate:
 		dx, dy, dz = DIRECTIONS[s]
 		x, y, z = x + dx, y + dy, z + dz
 	return x, y, z
-
-black_tiles = set()
+black_tiles: Set[Coordinate] = set()
 for coord in map(line_to_coordinate, data):
 	black_tiles ^= {coord}
+print(len(black_tiles))
+
+def neighbors(coord: Coordinate) -> Iterator[Coordinate]:
+	return (
+		cast(Coordinate, tuple(map(sum, zip(coord, d))))
+		for d in DIRECTIONS.values()
+	)
+def update(black_tiles: Set[Coordinate]) -> Set[Coordinate]:
+	def will_be_black(coord: Coordinate) -> bool:
+		n = sum(1 for neigh in neighbors(coord) if neigh in black_tiles)
+		return 1 <= n <= 2 if coord in black_tiles else n == 2
+	neighs = set(neigh for tile in black_tiles for neigh in neighbors(tile))
+	return set(filter(will_be_black, black_tiles | neighs))
+for _ in range(100):
+	black_tiles = update(black_tiles)
 print(len(black_tiles))
